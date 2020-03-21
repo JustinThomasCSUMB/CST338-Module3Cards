21bd243859db8cba54379f74bce67f18574135e8
JustinThomasCSUMB <justhomas@csumb.edu>
2020-03-21 16:43:10 -0700

    Working Phase 2

--- src/DeckOfCards.java
+++ src/DeckOfCards.java
@@ -1,3 +1,12 @@
+/**
+ * Group 4
+ * Justin Thomas
+ * Marcos Barrera
+ * Cristian Vazquez
+ * Michael Janes
+ * CST338 Module
+ */
+
 
 public class DeckOfCards
 {
@@ -8,16 +17,54 @@ public class DeckOfCards
       // Card Class Tests
       Card card1, illegalCard, card3;
       card1 = new Card();
-      System.out.println(card1.toString());
-      illegalCard = new Card('U', Card.Suit.diamonds);
-      System.out.println(illegalCard.toString());
+//      System.out.println(card1.toString());
+//      illegalCard = new Card('U', Card.Suit.diamonds);
+//      System.out.println(illegalCard.toString());
       card3 = new Card('j', Card.Suit.clubs);
-      System.out.println(card3.toString());
-
+//      System.out.println(card3.toString());
+      
+      
+      // Hand class debug code (phase 2)
+      // TODO: remove when finished
+      Hand myHand = new Hand();
+      boolean success = true;
+       
+      System.out.println("Filling Hand");
+      for(int i = 0; i < 100; i++) {                 
+         success = myHand.takeCard(card1);
+         success = myHand.takeCard(card3);
+         
+         if(!success) {
+            System.out.println("HandFull");
+            break;
+         }
+      }
+      
+      System.out.println("Testing inspectCard()");
+      Card inspect1 = myHand.inspectCard(10);
+      Card inspect2 = myHand.inspectCard(-1);
+      
+      System.out.println(inspect1.toString());
+      System.out.println(inspect2.toString());
+      
+      System.out.println("Printing full hand");
+      System.out.println(myHand.toString());
+      
+      for(int i = 0; i < 52; i++) {
+         System.out.println(myHand.playCard().toString());
+      }
+         
+      System.out.println("Printing empty hand");
+      System.out.println(myHand.toString());
+       // end phase 2 test code   
+      
+      
       // Card class equality check
-      Card identicalCard3 = new Card('J', Card.Suit.clubs);
-      System.out.println(card1.equals(identicalCard3));
-      System.out.println(identicalCard3.equals(card3));
+      /*
+       * Card identicalCard3 = new Card('J', Card.Suit.clubs);
+       * System.out.println(card1.equals(identicalCard3));
+       * System.out.println(identicalCard3.equals(card3));
+       */
       // End Card Class Tests
    }
 
